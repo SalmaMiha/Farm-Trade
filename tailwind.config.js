4d321746c4e5557cde8063701a41bf8be346ec3d
SalmaMiha <salma.islam.1999@gmail.com>
2023-11-05 12:11:38 +0600

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        jost: "'Jost', sans-serif",
+        quicksand: "'Quicksand', sans-serif",
+      },
+      colors:{
+        green: '#360',
+        grey: '#7D7C7C',
+        lightGrey: '#F1EFEF',
+        beige: '#ECE3CE'
+      },
+    },
+  },
+  plugins: [],
+}
+
