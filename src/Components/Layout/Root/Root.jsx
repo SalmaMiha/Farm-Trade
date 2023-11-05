a43063a0af9c1438c5abec6aed5e632a9dfc2604
SalmaMiha <salma.islam.1999@gmail.com>
2023-11-06 00:57:03 +0600

    footer added

--- src/Components/Layout/Root/Root.jsx
+++ src/Components/Layout/Root/Root.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../Shared/Navbar/Navbar";
+import Footer from "../../Shared/Footer/Footer";
 
 const Root = () => {
   return (
@@ -8,6 +9,7 @@ const Root = () => {
         <Navbar></Navbar>
       </div>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
